fc82aed1bd7b5aa7caabaaf8141538a1c525c226
Szymon Szysz <szysz.szymon@gmail.com>
2021-10-08 22:29:31 +0200

    Add text message view

---
+++ MillenniumPass/Text Message/TextMessageView.swift
@@ -0,0 +1,57 @@
+//
+//  TextMessageView.swift
+//  MillenniumPass
+//
+//  Created by Szymon Szysz on 08/10/2021.
+//
+
+import SwiftUI
+
+struct TextMessageView: View {
+    @State var smsString: String = ""
+    @State private var showingAlert = false
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color.white, Color(.magenta)]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.vertical)
+            .overlay(
+                VStack {
+                    Text("Poczekaj na wiadomość SMS z kodem PIN, aby aktywować aplikacje")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .multilineTextAlignment(.center)
+                    TextField("Kod sms", text: $smsString)
+                        .font(.system(.headline, design: .rounded))
+                        .multilineTextAlignment(.center)
+                        .padding(20)
+                    Spacer()
+                    Button("Autoryzuj".uppercased()) {
+                        if smsString == "1111" {
+                            print("You shall pass jeden")
+                        } else if smsString == "2222" {
+                            print("You shall pass dwa")
+                        } else {
+                            showingAlert = true
+                        }
+                    }
+                    .foregroundColor(.black)
+                    Spacer()
+                    Text("Copyright © Bank Millennium SA")
+                        .font(.system(.subheadline, design: .rounded))
+                        .foregroundColor(.white)
+
+                }
+                .padding([.top, .horizontal])
+                .padding(.horizontal)
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("Noooooo!"), message: Text("You shall not pass !!!"), dismissButton: .cancel())
+                }
+            )
+    }
+}
+
+struct TextMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextMessageView()
+    }
+}
+
