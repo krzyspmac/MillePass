ddeffa57df82ebe7fbcfdf77557ccc802212ad76
Krzysztof Pawłowski <krzyspmac@gmail.com>
2021-10-08 15:15:13 +0200

    Initial comit

---
+++ MillenniumPass/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MillenniumPass
+//
+//  Created by krzysp on 08/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
