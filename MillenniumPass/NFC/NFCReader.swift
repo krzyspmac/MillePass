24b7edf6b492b1d67960b76fe3c1557f74f4d38d
Krzysztof Pawłowski <krzyspmac@gmail.com>
2021-10-09 04:44:42 +0200

    Unauthorized correclty shows an error

--- MillenniumPass/NFC/NFCReader.swift
+++ MillenniumPass/NFC/NFCReader.swift
@@ -55,6 +55,7 @@ final class NFCReader: NSObject {
         case invalidAID(NFCISO7816Tag)
         case couldNotConnectToTag(NFCISO7816Tag)
         case sessionInvalidate(Error)
+        case notAuthorized
         case end
     }
 
@@ -74,8 +75,8 @@ final class NFCReader: NSObject {
         session?.begin()
     }
 
-    func endReading() {
-        invalidateSession(error: nil)
+    func endReading(error: TagReadingError? = nil) {
+        invalidateSession(error: error)
     }
 
     private func connectToTag(_ tag: NFCTag) {
@@ -87,13 +88,20 @@ final class NFCReader: NSObject {
                 return
             }
 
-            let userIdentifier = UIDevice.current.identifierForVendor!.uuidString
+            let userIdentifier = "asdds"//UIDevice.current.identifierForVendor!.uuidString
 
             self.tagInfo = TagInfo(tag: tag)
             self.sendAuthorization(with: userIdentifier) { state in
                 DispatchQueue.main.async {
                     self.state = state ?? .idle
-                    self.endReading()
+                    switch self.state {
+                    case .idle:
+                        self.endReading()
+                    case .loggedIn:
+                        self.endReading()
+                    case .unauthorized:
+                        self.endReading(error: .notAuthorized)
+                    }
                 }
             }
         }
@@ -186,6 +194,8 @@ fileprivate extension NFCReader.TagReadingError {
                 return "couldNotConnectToTag"
             case .sessionInvalidate(_):
                 return "sessionInvalidate"
+            case .notAuthorized:
+                return "Unauthorized"
             case .end:
                 return "end"
         }
